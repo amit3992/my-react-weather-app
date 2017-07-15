ae6bb8d7cb124bbfd983e3dae6040da605b36d17
Amit Kulkarni <amit3992@gatech.edu>
2017-07-15 01:19:56 -0700

    updated badass welcome message

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Amit's badass React weather app</h2>
+          <h2>Welcome to my badass React weather app</h2>
         </div>
 
         <div className = "search_bar">
